a5b91986470906ea7f8c44cdc176f610ca88b01e
Bengt Bäverman <bengt@baverman.se>
2021-09-30 18:59:47 +0200

    Update Body.js
    Just make a change for a change

--- src/Body.js
+++ src/Body.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Body = () => {
     return (
-        <p>Yes, this is just a <b>simple</b> body of text. There is nothing to see here.</p>
+        <p>Me oh my, this is just a <b>simple</b> body of text. There is nothing to see here.</p>
     )
 }
 
