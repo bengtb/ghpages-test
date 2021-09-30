835c8409ea269c31b6245fa856f722053306113d
Bengt Bäverman <bengt@baverman.se>
2021-09-30 18:58:29 +0200

    These are the files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Body from "./Body"
+
+function App() {
+  return (
+    <>
+    <h1>Smallest possible React test</h1>
+    <Body/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = () => {
+    return (
+        <p>Yes, this is just a <b>simple</b> body of text. There is nothing to see here.</p>
+    )
+}
+
+export default Body
